18c6679668abad7547afb3675c25da1d3c427762
Dashtiss <brandontisserand@outlook.com>
2023-09-14 19:37:48 -0500

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+using PlayerRoles;
+using System.ComponentModel;
+
+public class Config : IConfig
+{
+    public bool IsEnabled { get; set; } = true;
+    [Description("If debug mode is enabled. When enabled, the plugin will log debug messages to the console. DON'T USE WITH MORE THEN 1 PLAYER, CONSOLE WILL FLOOD.")]
+    public bool Debug { get; set; }
+}
---
+++ Lightning-Sl-Plugin.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API;
+using Exiled.API.Features;
+using Exiled.Events.Handlers;
+namespace Lightning_SL_Plugin
+{
+    public class Plugin : Plugin<Config>
+    {
+        private bool Experimental = true;
+        public override string Name => "Lightning-SL-Plugin";
+        public override string Author => "Dashtiss, Lightning-MC";
+        public override Version Version => new Version(1, 0, 0);
+        public override void OnEnabled()
+        {
+            Log.Warn("Lightning-sl-Plugin is empty");
+            if (Experimental)
+            {
+                Log.Warn("This is an experimental Version of Lightning-Sl Plugin");
+            }
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+
+    }
+}
