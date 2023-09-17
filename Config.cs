eba948cc441e87405293b20f1c48b36db9f881f7
Dashtiss <brandontisserand@outlook.com>
2023-09-17 17:54:12 -0500

    Updated the plugin to show if roundlock is on

--- Config.cs
+++ Config.cs
@@ -1,12 +1,13 @@
 ﻿using Exiled.API.Interfaces;
-using PlayerRoles;
 using System.ComponentModel;
 
 public class Config : IConfig
 {
+    [Description("If the plugin is enabled or not")]
     public bool IsEnabled { get; set; } = true;
-    [Description("If debug mode is enabled. When enabled, the plugin will log debug messages to the console. DON'T USE WITH MORE THEN 1 PLAYER, CONSOLE WILL FLOOD.")]
     public bool Debug { get; set; }
-
-    public string CassieTest { get; set; } = "This is a test of cassie";
+    [Description("broadcast if round lock is off")]
+    public string RLDisabledMessage { get; set; } = "Round Lock is off";
+    [Description("Broadcast if roundlock is on")]
+    public string RLEnabledMessage { get; set; } = "Round lock is on";
 }
--- Lightning-Sl-Plugin.cs
+++ Lightning-Sl-Plugin.cs
@@ -1,27 +1,46 @@
-﻿using Exiled.API;
-using Exiled.API.Features;
-using Exiled.Events.Handlers;
+﻿using Exiled.API.Features;
+using System;
+using Exiled.Permissions.Extensions;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Lightning_SL_Plugin
 {
     public class Plugin : Plugin<Config>
     {
-        private bool Experimental = true;
-        public override string Name => "Lightning-SL-Plugin";
-        public override string Author => "Dashtiss, Lightning-MC";
-        public override Version Version => new Version(1, 0, 1);
+        public override string Name { get; } = "Lightning-SL Plugin";
+        public override string Prefix { get; } = "LSP";
+        public override string Author { get; } = "Dashtiss";
+        public override Version Version { get; } = new Version(1, 0, 0);
         public override void OnEnabled()
         {
-            Log.Warn("Lightning-sl-Plugin is empty");
-            if (Experimental)
-            {
-                Log.Warn("This is an experimental Version of Lightning-Sl Plugin");
-            }
+            Timing.RunCoroutine(Timer());
             base.OnEnabled();
-        }
-        public override void OnDisabled()
-        {
-            base.OnDisabled();
+
         }
 
+        public override void OnDisabled()
+        { 
+            base.OnDisabled();
+        }
+        public IEnumerator<float> Timer()
+        {
+            if (Round.IsLocked)
+            {
+                foreach (Player pl in Player.List.Where(pl => pl.CheckPermission("et.roundlockinfo")))
+                {
+                    pl.Broadcast(3, Config.RLEnabledMessage, Broadcast.BroadcastFlags.AdminChat);
+                }
+            }
+            else
+            {
+                foreach (Player pl in Player.List.Where(pl => pl.CheckPermission("et.roundlockinfo")))
+                {
+                    pl.Broadcast(3, Config.RLDisabledMessage, Broadcast.BroadcastFlags.AdminChat);
+                }
+            }
+            yield return Timing.WaitForSeconds(1f);
+        }
     }
 }
